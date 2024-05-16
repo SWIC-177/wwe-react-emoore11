f065813aad4a19211f65338f765d7564e93bd55c
Erina <erina.moore@student.swic.edu>
2024-05-15 21:22:25 -0500

    Write props to create columns

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,3 +1,21 @@
-export default function Table() {
-  return <h2>Table</h2>;
+export default function Table({ col1, col2, col1Header, col2Header }) {
+  return (
+    <table className="container mx-auto max-w-fit text-left">
+      <thread>
+        <tr>
+          <th>{col1Header}</th>
+          <th>{col2Header}</th>
+        </tr>
+      </thread>
+
+      <tbody>
+        {col1.map((item, index) => (
+          <tr key={index}>
+            <td>{item}</td>
+            <td>{col2[index] || ""} </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
